9ea49b2a808556948dd6a0c2609bfe21b2d55338
Otavio Cardoso <otavio.cardoso@rfn.ptc>
2025-05-14 17:53:02 -0300

    fix: melhoria tratamento de erro index.ts

--- index.ts
+++ index.ts
@@ -2,15 +2,20 @@ import { AppDataSource } from './src/data-source';
 import express from 'express';
 import routes from './src/routes/routes';
 
-AppDataSource.initialize().then(() => {
-  const app = express();
-  app.use(express.json());
-  
-  app.use(routes);
-  
-  const PORT = process.env.NODE_PORT as number | undefined;
+AppDataSource.initialize()
+  .then(() => {
+    const app = express();
+    app.use(express.json());
+    
+    app.use(routes);
+    
+    const PORT = process.env.NODE_PORT as number | undefined;
 
-  return app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    return app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Erro ao iniciar a aplicação:', error);
+    process.exit(1);
   });
-});
