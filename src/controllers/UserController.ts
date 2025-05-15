64f46d8e6a6daa3e44df41cfc1529e8496c0527d
Otavio <otavio.zerotwo@gmail.com>
2025-05-14 23:25:52 -0300

    feat: add função login UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { userRepository } from '../repositories/userRepository';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export class UserController {
   async create(req: Request, res: Response): Promise<any>{
@@ -38,5 +39,27 @@ export class UserController {
       console.log('Erro na criação do usuário:', error);
       return res.status(500).json({ message: 'Erro interno do servidor' });
     }
+  }
+
+  async login(req: Request, res: Response): Promise<any>{
+    const { email, password } = req.body;
+
+    const user = await userRepository.findOneBy({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: 'E-mail ou senha inválidos' });
+    }
+
+    const verifyPass = await bcrypt.compare(password, user.password);
+
+    if (!verifyPass) {
+      return res.status(400).json({ message: 'E-mail ou senha inválidos' });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET ?? '', { expiresIn: '8h' });
+
+    const { password: _, ...userLogin } = user;
+
+    return res.json({ user: userLogin, token });
   }
 }
