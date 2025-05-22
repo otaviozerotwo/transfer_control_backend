9615167b38af23e924e7a4f71df89c24e5cb6e0f
Otavio <otavio.zerotwo@gmail.com>
2025-05-22 20:41:34 -0300

    fix: add cors middleware

--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
 import { AppDataSource } from './src/data-source';
 import express from 'express';
 import routes from './src/routes/routes';
+import cors from 'cors';
 
 AppDataSource.initialize()
   .then(() => {
     const app = express();
     app.use(express.json());
+
+    app.use(cors({
+      origin: 'https://transfer-control-reset-pass-page.vercel.app',
+      credentials: true
+    }));
     
     app.use(routes);
     
